27b238cdf7c3905648de4b35aa6b44b97c80b4d9
Pratyush Harsh <pratyush9000@gmail.com>
2020-03-08 14:56:53 +0530

    Fixed typos

--- kernel challenge/ex07_simple_module.c
+++ kernel challenge/ex07_simple_module.c
@@ -2,27 +2,29 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 
-int ex07(struct inode *pinode,struct file *pfile)
+int ex07_open(struct inode *pinode,struct file *pfile)
 {
-	printk(KERN_ALERT "inside the function function ex07 open");
+	printk(KERN_ALERT "inside the function function ex07 open\n");
+	return 0;
 
 }
-ssize_t ex07_read(struct file 8pfile,char __user *buffer,size_t length,loff_t *offset)
+ssize_t ex07_read(struct file *pfile,char __user *buffer,size_t length,loff_t *offset)
 {
-        printk(KERN_ALERT "Inside the function %sfunction\n",__FUNCTION__);
+        printk(KERN_ALERT "Inside the function read function\n");
+	return 0;
 }
-ssize_t ex07_write(struct *pfile.const char __user *buffer,size_t length,loff_t *offset)
+ssize_t ex07_write(struct file *pfile,const char __user *buffer,size_t length,loff_t *offset)
 {
-	printk(KERN_ALERT "Inside the function %s\n",__FUNCTION__);
+	printk(KERN_ALERT "Inside the function write\n");
 	return length;
 }
 int ex07_close(struct inode *pinode,struct file *pfile)
 {
-	printk(KERN_ALERT "Inside the function %s\n",__FUNCTION	__);
+	printk(KERN_ALERT "Inside the function close\n");
 	return 0;
 }
 
-struct file_operations_ex07_file_operations={
+struct file_operations ex07_file_operations={
 	.owner=THIS_MODULE,
 	.open =ex07_open,
 	.read=ex07_read,
@@ -32,13 +34,14 @@ struct file_operations_ex07_file_operations={
 };
 int ex01_simple_module_init(void)
 {
-	printk(KERN_ALERT "Inside the %s ffunction\n"__FUNCTION__);
+	printk(KERN_ALERT "Inside the init function\n");
 	//register the character device driver in the kernel
-	register_chardev(240/*major num*/,"Simple Character driver by pratyush",&ex07_file_operations/*file operations*/);
+	register_chrdev(240/*major num*/,"Simple Character driver by pratyush",&ex07_file_operations/*file operations*/);
+	return 0;
 }
-void exo1_simple_module_exit(void)
+void ex01_simple_module_exit(void)
 {
-	printk(KERN_ALERT "Inside the %s function\n",__FUNCTION__);
+	printk(KERN_ALERT "Inside the exit function\n");
 	//unrigester the character device driver
 	unregister_chrdev(240,"Simple Chararacter driver by pratyush");
 
